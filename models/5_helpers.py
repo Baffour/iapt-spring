bc633c11c9b36c7fd6e8d2601bc407252abbb0fa
Baffour <Baffour@users.noreply.github.com>
2016-03-03 02:17:16 +0000

    Added function to generate breadcrumbs (can be called directly from to view)

--- models/5_helpers.py
+++ models/5_helpers.py
@@ -29,3 +29,37 @@ def load_unfiled_box():
 def items_in(box):
     items_and_boxes = db((db.box.id==db.itm2box.box) & (db.itm2box.itm==db.itm.id) & (db.box.id==box.id))
     return items_and_boxes.select(db.itm.ALL)
+
+def breadcrumbs(arg_title=None):
+   "Create breadcrumb links for current request"
+   # source:http://www.web2pyslices.com/slice/show/1373/easy-breadcrumbs
+   # make links pretty by capitalizing and using 'home' instead of 'default'
+   pretty = lambda s: s.replace('default', 'Início').replace('_', ' ').capitalize()
+   menus = [A(T('Home'), _href=URL(r=request, c='default', f='index'))]
+   if request.controller != 'default':
+       # add link to current controller
+       menus.append(A(T(pretty(request.controller)), _href=URL(r=request, c=request.controller, f='index')))
+       if request.function == 'index':
+           # are at root of controller
+           menus[-1] = A(T(pretty(request.controller)), _href=URL(r=request, c=request.controller, f=request.function))
+       else:
+           # are at function within controller
+           menus.append(A(T(pretty(request.function)), _href=URL(r=request, c=request.controller, f=request.function)))
+       # you can set a title putting using breadcrumbs('My Detail Title')
+       if request.args and arg_title:
+           menus.append(A(T(arg_title)), _href=URL(r=request, c=request.controller, f=request.function,args=[request.args]))
+   else:
+       #menus.append(A(pretty(request.controller), _href=URL(r=request, c=request.controller, f='index')))
+       if request.function == 'index':
+           # are at root of controller
+           #menus[-1] = pretty(request.controller)
+           pass
+           #menus.append(A(pretty(request.controller), _href=URL(r=request, c=request.controller, f=request.function)))
+       else:
+           # are at function within controller
+           menus.append(A(T(pretty(request.function)), _href=URL(r=request, c=request.controller, f=request.function)))
+       # you can set a title putting using breadcrumbs('My Detail Title')
+       if request.args and arg_title:
+           menus.append(A(T(arg_title), _href=URL(r=request, f=request.function,args=[request.args])))
+
+   return XML(' > '.join(str(m) for m in menus))
